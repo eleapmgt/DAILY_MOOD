704a4bfd02051c4c9b8e0167be5e4ce552239bd5
oceane seguy <oceane.seguy@kedgebs.com>
2022-08-30 11:42:09 +0200

    Programed edit and update method for diaries_controller

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,11 +1,26 @@
 class DiariesController < ApplicationController
+  before_action :set_diary, only: %i[edit update]
 
   def index
   end
 
   def edit
+    @diary = Diary.find(params[:id])
   end
 
   def update
+    @diary = Diary.find(params[:id])
+    @diary.update(diary_params)
+    redirect_to diary_path(@diary)
+  end
+
+  private
+
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
+
+  def diary_params
+    params.require(:diary).permit(:gratitude, :date, :user_id)
   end
 end
