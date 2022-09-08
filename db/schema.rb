4735ebf8fdfd52b40d6f2a4f454b49cd9d10a4f4
sherazade othman <othmansherazade@gmail.com>
2022-09-08 10:41:50 +0200

    moods slider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_104955) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_084221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,4 +93,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_104955) do
   add_foreign_key "moods", "diaries"
   add_foreign_key "user_categories", "categories"
   add_foreign_key "user_categories", "users"
+  add_foreign_key "users", "users", column: "doctor_id"
 end
